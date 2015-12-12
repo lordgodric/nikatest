525cda8def910621b5f00d11bbbe89b45a4bae2c
lordgodric <elric@ex.ua>
2015-12-13 01:13:03 +0200

    yourCity class in progress

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
+	// Чекбокс
 	$(".input-checkbox").click(function(){
 		// Видалимо всі обрання із елементів input-checkbox 
 		$(".input-checkbox").removeClass("checked"); 
 		// Добавимо обраному елементу клас checked
        	$(this).addClass("checked");
-       	
-       	
     });
+
+	// Перемикання кнопки "Свернуть" на "Розвернуть" і змінна іконки
+	$(".toggle").click(function(){
+    	$(".middle").slideToggle(400);
+    	if ($(".toggle-map").text() == 'Свернуть ') {
+    		$(".toggle-map").text('Развернуть ')
+    		$(".hide-img").attr('src','../img/down-arrowhead.png');
+    	} else if($(".toggle-map").text() == 'Развернуть '){
+    		$(".toggle-map").text('Свернуть ')
+    		$(".hide-img").attr('src','../img/up-arrowhead.png');
+    	}
+    });
+    
+
+    
+    
 });
