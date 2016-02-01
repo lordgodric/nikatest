0f0fd98d133e97d4e175fff3ba735a1aaa5fd8af
lordgodric <elric@ex.ua>
2016-02-01 12:01:07 +0200

    add .footer-text

--- js/checkbox.js
+++ js/checkbox.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	// Чекбокс
 	$(".input-checkbox").click(function(){
-		// Видалимо всі обрання із елементів input-checkbox 
-		$(".input-checkbox").removeClass("checked"); 
+		// Видалимо всі обрання із елементів input-checkbox
+		$(".input-checkbox").removeClass("checked");
 		// Добавимо обраному елементу клас checked
        	$(this).addClass("checked");
     });
@@ -10,15 +10,15 @@ $(document).ready(function() {
 	// Перемикання кнопки "Свернуть" на "Розвернуть" і змінна іконки
 	$(".toggle").click(function(){
     	$(".middle").slideToggle(400);
-    	if ($(".toggle-map").text() == 'Свернуть ') {               // якщо стоїть значення згорнути то 
+    	if ($(".toggle-map").text() == 'Свернуть ') {               // якщо стоїть значення згорнути то
     		$(".toggle-map").text('Развернуть ')                    // змінюємо на значення розгорнути
-    		$(".hide-img").attr('src','../img/down-arrowhead.png'); // і замінюємо картинку стрілки
+    		$(".hide-img").attr('src','img/down-arrowhead.png'); // і замінюємо картинку стрілки
     	} else if($(".toggle-map").text() == 'Развернуть '){
     		$(".toggle-map").text('Свернуть ')
-    		$(".hide-img").attr('src','../img/up-arrowhead.png');
+    		$(".hide-img").attr('src','img/up-arrowhead.png');
     	}
     });
-    
+
     // Вибір міста зі списку
     $(".cityList").children().click(function(){                     // клік на дочірнюму елементу списка
         $(".favoritesCity").text($(this).text())                    // добавимо значення обраного елемента
@@ -28,14 +28,13 @@ $(document).ready(function() {
     // Видалення міста із обраного
     $(".closeArrow").click(function(){                              // клік по стрілці видаллення міста із обраного
         $(".favoritesCity").empty();                                // очищення значення обраного міста
-        $(".closeArrow").css('display', 'none');                    // приховуваня стрілки
+        $(".closeArrowForWhom").css('display', 'none');                    // приховуваня стрілки
     });
 
-    // Значок видалення міста із обраного - показувати лише тоді коли місто обране
-    // if (!$(".favoritesCity").val()) {
-    //     console.log('testt');
-    //     $(".closeArrow").css('display', 'none');
-    // }
-    
-    
+    $(".forWhom").children().click(function(){                      // клік на дочірнюму елементу списка
+        $(".person").text($(this).text())                           // добавимо значення обраного елемента
+        $(".closeArrow").css('display', 'inline-block');            // воводимо стрілку видалення міста із обраного (по замовчуванню воно прихована)
+    });
+
+
 });
