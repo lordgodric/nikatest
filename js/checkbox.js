92f5d22e2bce1f95816d4384aecb4e7c5fa1024e
lordgodric <elric@ex.ua>
2016-02-04 12:04:46 +0200

    Зявилася горизонтальна полоска

--- js/checkbox.js
+++ js/checkbox.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
 	       	$(this).addClass("checked");
 	    });
 
+
+
 	// Перемикання кнопки "Свернуть" на "Розвернуть" і змінна іконки
 	$(".toggle").click(function(){
     	$(".middle").slideToggle(400);
@@ -20,6 +22,8 @@ $(document).ready(function() {
     	}
     });
 
+
+
     // Вибір міста зі списку
     $(".cityList").children().click(function(){                     // клік на дочірнюму елементу списка
         $(".favoritesCity").text($(this).text())                    // добавимо значення обраного елемента
@@ -28,61 +32,115 @@ $(document).ready(function() {
     // Видалення міста із обраного
     $(".closeArrow").click(function(){                              // клік по стрілці видаллення міста із обраного
         $(".favoritesCity").empty();                                // очищення значення обраного міста
-        $(".closeArrow").css('display', 'none');							// приховуваня стрілки
+        $(".closeArrow").css('display', 'none');										// приховуваня стрілки
     });
 
 		// Вибір "Для кого"
-    $(".forWhomList").children().click(function(){                      // клік на дочірнюму елементу списка
-        $(".alreadyChosedForWhom").text($(this).text())                           // добавимо значення обраного елемента
-        $(".closeArrowForWhom").css('display', 'inline-block');            // воводимо стрілку видалення міста із обраного (по замовчуванню воно прихована)
+    $(".forWhomList").children().click(function(){                  // клік на дочірнюму елементу списка
+        $(".alreadyChosedForWhom").text($(this).text())             // добавимо значення обраного елемента
+        $(".closeArrowForWhom").css('display', 'inline-block');     // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-		// Видалення міста із поля "Для кого"
-    $(".closeArrowForWhom").click(function(){                              // клік по стрілці видаллення міста із обраного
-        $(".alreadyChosedForWhom").empty();                                // очищення значення обраного міста
-        $(".closeArrowForWhom").css('display', 'none');							// приховуваня стрілки
+		// Видалення елемента із поля "Для кого"
+    $(".closeArrowForWhom").click(function(){                       // клік по стрілці видаллення елемента із обраного
+        $(".alreadyChosedForWhom").empty();                         // очищення значення обраного елемента
+        $(".closeArrowForWhom").css('display', 'none');				// приховуваня стрілки
     });
 
 		// Вибір "Металу корпусу"
     $(".forCaseMetal").children().click(function(){                      // клік на дочірнюму елементу списка
-        $(".alreadyChosedForCaseMetal").text($(this).text())                           // добавимо значення обраного елемента
-        $(".closeArrowForCaseMetal").css('display', 'inline-block');            // воводимо стрілку видалення міста із обраного (по замовчуванню воно прихована)
+        $(".alreadyChosedForCaseMetal").text($(this).text())             // добавимо значення обраного елемента
+        $(".closeArrowForCaseMetal").css('display', 'inline-block');     // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-		// Видалення міста із поля "Матал корпусу"
-    $(".closeArrowForCaseMetal").click(function(){                              // клік по стрілці видаллення міста із обраного
-        $(".alreadyChosedForCaseMetal").empty();                                // очищення значення обраного міста
-        $(".closeArrowForCaseMetal").css('display', 'none');							// приховуваня стрілки
+		// Видалення елемента із поля "Матал корпусу"
+    $(".closeArrowForCaseMetal").click(function(){                       // клік по стрілці видаллення елемента із обраного
+        $(".alreadyChosedForCaseMetal").empty();                         // очищення значення обраного елемента
+        $(".closeArrowForCaseMetal").css('display', 'none');			 // приховуваня стрілки
     });
 
 		// Вибір "Кольору"
-    $(".forColor").children().click(function(){                      // клік на дочірнюму елементу списка
-        $(".alreadyChosedForColor").text($(this).text())                           // добавимо значення обраного елемента
-        $(".closeArrowForColor").css('display', 'inline-block');            // воводимо стрілку видалення міста із обраного (по замовчуванню воно прихована)
+    $(".forColor").children().click(function(){                      	 // клік на дочірнюму елементу списка
+        $(".alreadyChosedForColor").text($(this).text())                 // добавимо значення обраного елемента
+        $(".closeArrowForColor").css('display', 'inline-block');         // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-		// Видалення міста із поля "Кольору"
-    $(".closeArrowForColor").click(function(){                              // клік по стрілці видаллення міста із обраного
-        $(".alreadyChosedForColor").empty();                                // очищення значення обраного міста
-        $(".closeArrowForColor").css('display', 'none');							// приховуваня стрілки
+		// Видалення елемента із поля "Кольору"
+    $(".closeArrowForColor").click(function(){                           // клік по стрілці видаллення елемента із обраного
+        $(".alreadyChosedForColor").empty();                             // очищення значення обраного елемента
+        $(".closeArrowForColor").css('display', 'none');			     // приховуваня стрілки
     });
 
 		// Вибір "Вставок"
-    $(".forInsert").children().click(function(){                      // клік на дочірнюму елементу списка
-        $(".alreadyChosedForInsert").text($(this).text())                           // добавимо значення обраного елемента
-        $(".closeArrowForInsert").css('display', 'inline-block');            // воводимо стрілку видалення міста із обраного (по замовчуванню воно прихована)
+    $(".forInsert").children().click(function(){                      	 // клік на дочірнюму елементу списка
+        $(".alreadyChosedForInsert").text($(this).text())                // добавимо значення обраного елемента
+        $(".closeArrowForInsert").css('display', 'inline-block');        // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-		// Видалення міста із поля "Вставок"
-    $(".closeArrowForInsert").click(function(){                              // клік по стрілці видаллення міста із обраного
-        $(".alreadyChosedForInsert").empty();                                // очищення значення обраного міста
-        $(".closeArrowForInsert").css('display', 'none');							// приховуваня стрілки
+		// Видалення елемента із поля "Вставок"
+    $(".closeArrowForInsert").click(function(){                          // клік по стрілці видаллення елемента із обраного
+        $(".alreadyChosedForInsert").empty();                            // очищення значення обраного елемента
+        $(".closeArrowForInsert").css('display', 'none');			     // приховуваня стрілки
     });
+
+
 
 		// Закрити віко dropdown-menu
 		$(".dropdown-menu .closeDrowpdownMenu").click(function() {
     		$('.in,.open').removeClass('in open');
 		});
 
+
+
 		// Відкрити додаткове вікно для кожного годинника (під картою)
-		// $(".info").hide();
+        // Ховаємо всі обгортки блоку інформації
+		$(".info").hide();
+
+        // Ховаємо всі кнопки які розміщення поверх зображення годинників
+		$(".select-dial").hide();
+
+        // Клік на годдинику
 		$(".oneWatches").click(function(){
-			$(this).parent().next().toggle()
+
+            // зміна для обгортки для зображення годинників + розгортання і згортанню обгортки блоку інформації
+			var selectedImageWrapper = $(this).parent().next().toggle();
+
+            // розгортання і згортання обгортки блоку інформації для останнього контейнера годинників
+            $(this).parent().prev().toggle();
+
+            // перемикає показ кнопки на зображенню з годинниками
+			$(this).next().toggle();
+			
+            var selectedWatchesWrapper = selectedImageWrapper.parent();  							// обрана обгортка для зображення та вікна інформації
+			var lastSelectedWatchesWrapper = $(".watchesWrapper").last(); 						    // остання обгортка зі списку всих обгорток
+           
+            // порівняння обранної обгортки із останньою
+            // якщо вони рівні то робимо для неї щоб обгортка інформації і зображення мінялися місцями
+			if (selectedWatchesWrapper.get(0) == lastSelectedWatchesWrapper.get(0)) {               
+
+                
+                // змінна для останньої обгортки для зображення з годинниками
+                var watchesWrapper = $(lastSelectedWatchesWrapper).children(":first");
+
+                // змінна для останньої обгортки для додаткової інформації
+                var watchesInfoWrapper = $(lastSelectedWatchesWrapper).children(":last")
+
+                // змінна місцями 2-ох останніх обгорток
+                $(watchesInfoWrapper).insertBefore($(watchesWrapper))
+
+                // зміни в стилях для динамічних 2-ох останніх обгорток
+                $(lastSelectedWatchesWrapper).children(":last").css("margin-left", "0px")
+                $(lastSelectedWatchesWrapper).children(":first").css("margin-right", "15px")
+				$(lastSelectedWatchesWrapper).children(":first").css("margin-left", "0px")
+			}
 		});
+
+
+
+	// Змінна картинки при hover на блок новини
+	$(".single-text-block:nth-child(1)").hover(function(){
+			$(".news-image").css("background-image", "url(\"../img/news1.png\")");
+	});
+	$(".single-text-block:nth-child(2)").hover(function(){
+			$(".news-image").css("background-image", "url(\"../img/news2.png\")");
+	});
+	$(".single-text-block:nth-child(3)").hover(function(){
+			$(".news-image").css("background-image", "url(\"../img/news1.png\")");
+	});
 });
