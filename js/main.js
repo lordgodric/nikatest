717f4d51b1c2e350dd714b98a07e7d766c7437bd
lordgodric <elric@ex.ua>
2016-02-09 12:42:46 +0200

    try fixes navbar for ie8

--- js/main.js
+++ js/main.js
@@ -27,26 +27,26 @@ $(document).ready(function() {
     });
 
 
-	// Чекбокс
-	$(".input-checkbox").click(function(){
-			// Видалимо всі обрання із елементів input-checkbox
-			$(".input-checkbox").removeClass("checked");
-			// Добавимо обраному елементу клас checked
-	       	$(this).addClass("checked");
-	    });
+    // Чекбокс
+    $(".input-checkbox").click(function(){
+            // Видалимо всі обрання із елементів input-checkbox
+            $(".input-checkbox").removeClass("checked");
+            // Добавимо обраному елементу клас checked
+            $(this).addClass("checked");
+        });
 
 
 
-	// Перемикання кнопки "Свернуть" на "Розвернуть" і змінна іконки
-	$(".toggle").click(function(){
-    	$(".middle").slideToggle(400);
-    	if ($(".toggle-map").text() == 'Свернуть ') {                 // якщо стоїть значення згорнути то
-    		$(".toggle-map").text('Развернуть ')                        // змінюємо на значення розгорнути
-    		$(".hide-img").attr('src','img/down-arrowhead.png'); 			  // і замінюємо картинку стрілки
-    	} else if($(".toggle-map").text() == 'Развернуть '){
-    		$(".toggle-map").text('Свернуть ')
-    		$(".hide-img").attr('src','img/up-arrowhead.png');
-    	}
+    // Перемикання кнопки "Свернуть" на "Розвернуть" і змінна іконки
+    $(".toggle").click(function(){
+        $(".middle").slideToggle(400);
+        if ($(".toggle-map").text() == 'Свернуть ') {                 // якщо стоїть значення згорнути то
+            $(".toggle-map").text('Развернуть ')                        // змінюємо на значення розгорнути
+            $(".hide-img").attr('src','img/down-arrowhead.png');              // і замінюємо картинку стрілки
+        } else if($(".toggle-map").text() == 'Развернуть '){
+            $(".toggle-map").text('Свернуть ')
+            $(".hide-img").attr('src','img/up-arrowhead.png');
+        }
     });
 
 
@@ -59,65 +59,65 @@ $(document).ready(function() {
     // Видалення міста із обраного
     $(".closeArrow").click(function(){                              // клік по стрілці видаллення міста із обраного
         $(".favoritesCity").empty();                                // очищення значення обраного міста
-        $(".closeArrow").css('display', 'none');										// приховуваня стрілки
+        $(".closeArrow").css('display', 'none');                                        // приховуваня стрілки
     });
 
-	// Вибір "Для кого"
+    // Вибір "Для кого"
     $(".forWhomList").children().click(function(){                  // клік на дочірнюму елементу списка
         $(".alreadyChosedForWhom").text($(this).text());             // добавимо значення обраного елемента
         $(".closeArrowForWhom").css('display', 'inline-block');     // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-	// Видалення елемента із поля "Для кого"
+    // Видалення елемента із поля "Для кого"
     $(".closeArrowForWhom").click(function(){                       // клік по стрілці видаллення елемента із обраного
         $(".alreadyChosedForWhom").empty();                         // очищення значення обраного елемента
-        $(".closeArrowForWhom").css('display', 'none');				// приховуваня стрілки
+        $(".closeArrowForWhom").css('display', 'none');             // приховуваня стрілки
     });
 
-	// Вибір "Металу корпусу"
+    // Вибір "Металу корпусу"
     $(".forCaseMetal").children().click(function(){                      // клік на дочірнюму елементу списка
         $(".alreadyChosedForCaseMetal").text($(this).text());             // добавимо значення обраного елемента
         $(".closeArrowForCaseMetal").css('display', 'inline-block');     // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-	// Видалення елемента із поля "Матал корпусу"
+    // Видалення елемента із поля "Матал корпусу"
     $(".closeArrowForCaseMetal").click(function(){                       // клік по стрілці видаллення елемента із обраного
         $(".alreadyChosedForCaseMetal").empty();                         // очищення значення обраного елемента
-        $(".closeArrowForCaseMetal").css('display', 'none');			 // приховуваня стрілки
+        $(".closeArrowForCaseMetal").css('display', 'none');             // приховуваня стрілки
     });
 
-	// Вибір "Кольору"
-    $(".forColor").children().click(function(){                      	 // клік на дочірнюму елементу списка
+    // Вибір "Кольору"
+    $(".forColor").children().click(function(){                          // клік на дочірнюму елементу списка
         $(".alreadyChosedForColor").text($(this).text());                 // добавимо значення обраного елемента
         $(".closeArrowForColor").css('display', 'inline-block');         // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-	// Видалення елемента із поля "Кольору"
+    // Видалення елемента із поля "Кольору"
     $(".closeArrowForColor").click(function(){                           // клік по стрілці видаллення елемента із обраного
         $(".alreadyChosedForColor").empty();                             // очищення значення обраного елемента
-        $(".closeArrowForColor").css('display', 'none');			     // приховуваня стрілки
+        $(".closeArrowForColor").css('display', 'none');                 // приховуваня стрілки
     });
 
-	// Вибір "Вставок"
-    $(".forInsert").children().click(function(){                      	 // клік на дочірнюму елементу списка
+    // Вибір "Вставок"
+    $(".forInsert").children().click(function(){                         // клік на дочірнюму елементу списка
         $(".alreadyChosedForInsert").text($(this).text());                // добавимо значення обраного елемента
         $(".closeArrowForInsert").css('display', 'inline-block');        // воводимо стрілку видалення елемента із обраного (по замовчуванню воно прихована)
     });
-	// Видалення елемента із поля "Вставок"
+    // Видалення елемента із поля "Вставок"
     $(".closeArrowForInsert").click(function(){                          // клік по стрілці видаллення елемента із обраного
         $(".alreadyChosedForInsert").empty();                            // очищення значення обраного елемента
-        $(".closeArrowForInsert").css('display', 'none');			     // приховуваня стрілки
+        $(".closeArrowForInsert").css('display', 'none');                // приховуваня стрілки
     });
 
 
 
 
-	// Відкрити додаткове вікно для кожного годинника (під картою)
+    // Відкрити додаткове вікно для кожного годинника (під картою)
     // Ховаємо всі обгортки блоку інформації
-	$(".info").hide();
+    $(".info").hide();
 
     // Ховаємо всі кнопки які розміщення поверх зображення годинників
-	$(".select-dial").hide();
+    $(".select-dial").hide();
 
     // Клік на годдинику
-	$(".oneWatches").mouseover(function(){
+    $(".oneWatches").mouseover(function(){
 
         // знову закриємо всі обгорти для блоку інформації
         // тим самим ми робимо так що якщо вже відкрита якась обгортка
@@ -126,20 +126,20 @@ $(document).ready(function() {
         $(".select-dial").hide();
 
         // зміна для обгортки для зображення годинників + розгортання і згортанню обгортки блоку інформації
-		var selectedImageWrapper = $(this).parent().next().toggle();
+        var selectedImageWrapper = $(this).parent().next().toggle();
 
         // розгортання і згортання обгортки блоку інформації для останнього контейнера годинників
         $(this).parent().prev().toggle();
 
         // перемикає показ кнопки на зображенню з годинниками
-		$(this).next().toggle();
-		
-        var selectedWatchesWrapper = selectedImageWrapper.parent();  							// обрана обгортка для зображення та вікна інформації
-		var lastSelectedWatchesWrapper = $(".watchesWrapper").last(); 						    // остання обгортка зі списку всих обгорток
+        $(this).next().toggle();
+        
+        var selectedWatchesWrapper = selectedImageWrapper.parent();                             // обрана обгортка для зображення та вікна інформації
+        var lastSelectedWatchesWrapper = $(".watchesWrapper").last();                           // остання обгортка зі списку всих обгорток
        
         // порівняння обранної обгортки із останньою
         // якщо вони рівні то робимо для неї щоб обгортка інформації і зображення мінялися місцями
-		if (selectedWatchesWrapper.get(0) == lastSelectedWatchesWrapper.get(0)) {               
+        if (selectedWatchesWrapper.get(0) == lastSelectedWatchesWrapper.get(0)) {               
 
             // змінна для останньої обгортки для зображення з годинниками
             var watchesWrapper = $(lastSelectedWatchesWrapper).children(":first");
@@ -153,22 +153,22 @@ $(document).ready(function() {
             // зміни в стилях для динамічних 2-ох останніх обгорток
             $(lastSelectedWatchesWrapper).children(":last").css("margin-left", "0px")
             $(lastSelectedWatchesWrapper).children(":first").css("margin-right", "15px")
-			$(lastSelectedWatchesWrapper).children(":first").css("margin-left", "0px")
-		}
-	});
+            $(lastSelectedWatchesWrapper).children(":first").css("margin-left", "0px")
+        }
+    });
 
 
 
-	// Змінна картинки новини при hover на блок новини
-	$(".single-text-block:nth-child(1)").hover(function(){
-			$(".news-image").css("background-image", "url(\"../img/news1.png\")");
-	});
-	$(".single-text-block:nth-child(2)").hover(function(){
-			$(".news-image").css("background-image", "url(\"../img/news2.png\")");
-	});
-	$(".single-text-block:nth-child(3)").hover(function(){
-			$(".news-image").css("background-image", "url(\"../img/news1.png\")");
-	});
+    // Змінна картинки новини при hover на блок новини
+    $(".single-text-block:nth-child(1)").hover(function(){
+            $(".news-image").css("background-image", "url(\"../img/news1.png\")");
+    });
+    $(".single-text-block:nth-child(2)").hover(function(){
+            $(".news-image").css("background-image", "url(\"../img/news2.png\")");
+    });
+    $(".single-text-block:nth-child(3)").hover(function(){
+            $(".news-image").css("background-image", "url(\"../img/news1.png\")");
+    });
 
     
 
